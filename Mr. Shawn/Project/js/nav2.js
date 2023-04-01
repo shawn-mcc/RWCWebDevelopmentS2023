3c09fc113eae3e04cd28f746228a8ce8111c7ef0
Shawn McCausland <shawnmcc1999@gmail.com>
2023-04-01 14:32:04 -0400

    in-class actrivities from 4-1

---
+++ Mr. Shawn/Project/js/nav2.js
@@ -0,0 +1,20 @@
+function applyNavbar() {
+    var navbar = `<div class="collapse navbar-collapse" id="navbarSupportedContent"> <!-- Start of Navbar content-->
+    <ul class="navbar-nav mr-auto"> <!-- Start of Navbar List-->
+        <li class="nav-item active">
+            <!-- Notice how we use the Active class to show which page we are currently on -->
+            <a class="nav-link" href="#">Home</a> <!-- href = # means it will stay on the same page -->
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="my_family.html">My Family</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="my_hobbies.html">My Hobbies</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="my_classmates.html">My Classmates</a>
+        </li>
+    </ul> <!-- End of Navbar List-->
+</div> <!-- End of Navbar content-->`;
+document.getElementById("navbar-space").innerHTML = navbar;
+}
---
+++ Mr. Shawn/in-class-examples/Lesson 7 - Implementing Javascript/js/about_me.js
@@ -0,0 +1,8 @@
+const Shawn = {
+    first_name: "Shawn",
+    last_name: "McCausland",
+    favorite_color: "red",
+    favorite_song: "The Sound of Silence",
+    image_location: "https://drive.google.com/uc?export=download&id=1tZK-EybYwXL7IUfAhAodEG2qRZ4-OuaG",
+    site: "http://rwc-webdev-s2023.000webhostapp.com/Mr.%20Shawn/Project/index.html"
+}
